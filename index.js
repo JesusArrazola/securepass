35f125383fdbfe9cd4a643451e1f04debab4c893
=Jesus Arrazola <arrazola69@hotmail.com>
2023-07-31 21:45:33 -0500

    struct: Created folder structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Version routes
+const v1Router = require("./routes/v1/v1Routes");
+app.use("/v1", v1Router);
+
+//Server listennnig
+app.listen(PORT, () => {
+  console.log(`Server listenning at port ${PORT}`);
+});
