59e1c3e3d2191a794cd39a02c6c5c3d87a1f6d09
=Jesus Arrazola <arrazola69@hotmail.com>
2023-08-03 08:31:16 -0500

    v1Routes: Removed unused requires

--- routes/v1/v1Routes.js
+++ routes/v1/v1Routes.js
@@ -1,4 +1,3 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const generate = require("../../controllers/passwordGenerator");
 const { buildParamsObject } = require("../../helpers/parameterObject");
